63f4a991092cea4dd8cabadb146901acfa411927
julie1013 <julie1380@verizon.net>
2016-11-09 14:13:19 -0500

    Wrote CURL requests...
    ...for episodes and playlists.

--- scripts/episodes.sh
+++ scripts/episodes.sh
@@ -1,7 +1,23 @@
-curl --include --request PATCH http://localhost:4741/episodes/18 \
-  --header "Content-Type: application/json" \
+
+curl --include --request GET http://localhost:4741/episodes/
+
+
+curl --include --request POST https://localhost:4741/episodes \
+--header "Content-Type: application/json" \
   --data '{
     "episode": {
-      "title": "So It\u0027s Come to This: A Simpsosdfns Clip Show"
+      "title": "Some episode"
     }
   }'
+
+
+  curl --include --request PATCH http://localhost:4741/episodes/23 \
+    --header "Content-Type: application/json" \
+    --data '{
+      "episode": {
+        "title": "Some episode 2"
+      }
+    }'
+
+
+  curl --include --request DELETE https://localhost:4741/episodes/23
--- scripts/playlists.sh
+++ scripts/playlists.sh
@@ -1,5 +1,24 @@
+
+  curl --include --request POST https://localhost:4741/playlists/1 \
+  --header "Content-Type: application/json" \
+    --data '{
+      "episode": {
+        "user_id": "1",
+        "episode_id": "2",
+        "rating": "5"
+      }
+    }'
+
+curl --include --request GET http://localhost:4741/playlists/
+
+curl --include --request DELETE https://localhost:4741/playlists/1
+
 curl --include --request PATCH http://localhost:4741/playlists/1 \
   --header "Content-Type: application/json" \
   --data '{
-    "first_episode": "Kamp Krusty"
+    "episode": {
+      "user_id": "1",
+      "episode_id": "1",
+      "rating": "3"
+    }
   }'
